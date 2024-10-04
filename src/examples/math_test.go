30cee5cb731d1e4fae080c45a617dace4a02bbb9
sylhare <sylhare@outlook.com>
2024-10-04 12:02:38 -0400

    Add math example

---
+++ src/examples/math_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	result := Add(2, 3)
+	expected := 5
+
+	if result != expected {
+		t.Errorf("Add(2, 3) = %d; want %d", result, expected)
+	}
+}
