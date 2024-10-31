43fd6a0ee5da569333dbe03e0ba8d0122d645a4a
sylhare <sylhare@outlook.com>
2024-10-31 09:23:29 -0400

    Add server

---
+++ echo/app/server.go
@@ -0,0 +1,56 @@
+package app
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+func Server() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// Routing
+	e.POST("/users", SaveUser)
+	e.GET("/users/:id", GetUser)
+	e.GET("/show", Show)
+
+	// Handling request
+	e.POST("/users", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
+		// or
+		// return c.XML(http.StatusCreated, u)
+	})
+
+	// Root level middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Group level middleware
+	g := e.Group("/admin")
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "joe" && password == "secret" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	// Route level middleware
+	track := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			println("request to /users")
+			return next(c)
+		}
+	}
+	e.GET("/users", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users")
+	}, track)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
