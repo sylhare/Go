de556946912f5720f4ed92da4d96701f6e34b8f0
sylhare <sylhare@outlook.com>
2024-11-20 16:23:13 -0500

    Update generated files

--- pkg/sqlc-tutorial/cmd/tutorial.go
+++ pkg/sqlc-tutorial/cmd/tutorial.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"log"
 	"reflect"
-	db "tutorial.sqlc.dev/app/db/codegen"
+	db "tutorial.sqlc.dev/app/db/codegen/schema"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgtype"
