6c705e7d9da6a36d1a5443da69a3630ce7d3d635
sylhare <sylhare@outlook.com>
2024-12-04 09:44:15 -0500

    Update controller

--- echo/pkg/example/controller.go
+++ echo/pkg/example/controller.go
@@ -2,9 +2,7 @@ package example
 
 import (
 	"github.com/labstack/echo/v4"
-	"io"
 	"net/http"
-	"os"
 )
 
 // GetUser e.GET("/users/:id", GetUser)
@@ -28,35 +26,4 @@ func SaveUser(c echo.Context) error {
 	name := c.FormValue("name")
 	email := c.FormValue("email")
 	return c.String(http.StatusOK, "name:"+name+", email:"+email)
-}
-
-func save(c echo.Context) error {
-	// Get name
-	name := c.FormValue("name")
-	// Get avatar
-	avatar, err := c.FormFile("avatar")
-	if err != nil {
-		return err
-	}
-
-	// Source
-	src, err := avatar.Open()
-	if err != nil {
-		return err
-	}
-	defer src.Close()
-
-	// Destination
-	dst, err := os.Create(avatar.Filename)
-	if err != nil {
-		return err
-	}
-	defer dst.Close()
-
-	// Copy
-	if _, err = io.Copy(dst, src); err != nil {
-		return err
-	}
-
-	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
 }
